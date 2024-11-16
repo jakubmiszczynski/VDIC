50362e1d5fb0ed495c6ad5664d73e456ae85a9ea
jakubmiszczynski <miszczynski@student.agh.edu.pl>
2024-11-16 22:09:58 +0100

    lab02

---
+++ README.md
@@ -0,0 +1,1 @@
+# VIDIC_git
---
+++ xcelium.d/run.lnx86.23.03.d/files.ts
@@ -0,0 +1,4 @@
+1728643139 /home/student/jmiszczynski/VDIC/common/fifomult2024.svp
+1731359907 /home/student/jmiszczynski/VDIC/lab04/tb/fifomult_tb_pkg.svh
+1731360578 /home/student/jmiszczynski/VDIC/lab04/tb/fifomult_bfm.sv
+1731360364 /home/student/jmiszczynski/VDIC/lab04/tb/top.sv
